8b48529411352c214f25551b131d9fe3414b45ec
Tony She <tony.she@outlook.com>
2020-12-20 23:31:42 -0800

    fixed a door bug

--- src/gameFunctions/actions/goDoor.js
+++ src/gameFunctions/actions/goDoor.js
@@ -4,6 +4,7 @@ const moveObjs = require("../objectFunctions/moveObjectFunctions")
 
 async function goDoor(roomName, userId, doorName) {
     let doors = await getObjs.getAllDoorsInRoom(roomName)
+    doors = doors.filter((door) => {return door.names.includes(doorName)})
     if (doors.length > 1) {
         // more than one door by that name
         return 'There are more than one thing by the name ' + '"' + doorName + '." Please be more specific as to which one you mean.'
@@ -15,12 +16,14 @@ async function goDoor(roomName, userId, doorName) {
         if (door.locked) {
             return "It's locked."
         } else if (door.closed) {
-            return "It's closed. You should open it first"
+            return "It's closed. You should open it first."
         } else {
             // user goes through the door
             let toRoomName = door.toRoom
             await moveObjs.moveUserIdToAnotherRoom(userId, toRoomName)
-            return "You go through the " + doorName
+            // kind of janky. expects at least 2 names, with the first being the common name of the door
+            // and the second name being the common name of the destination.
+            return "You go through the " + door.names[0] + " to the " + door.names[1] + "."
         }
     }
 }
