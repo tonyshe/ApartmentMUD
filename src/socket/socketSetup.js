c801ac2b1a1c263e734aff71b1a1c8279a8679b7
Tony She <tony.she@outlook.com>
2021-01-01 21:58:24 -0800

    added queueing via bee-queue

--- src/socket/socketSetup.js
+++ src/socket/socketSetup.js
@@ -10,29 +10,57 @@ const getUsers = require('../gameFunctions/userFunctions/getUserFunctions')
 const setObjs = require('../gameFunctions/objectFunctions/setObjectFunctions')
 const moveObjs = require('../gameFunctions/objectFunctions/moveObjectFunctions')
 const textHelpers = require("../gameFunctions/helperFunctions/textHelpers")
-const {lookFunctions} = require("../gameFunctions/describeFunctions/describeFunctions")
-const {helpResponse} = require("../gameFunctions/actions/showHelp")
+const { lookFunctions } = require("../gameFunctions/describeFunctions/describeFunctions")
+const { helpResponse } = require("../gameFunctions/actions/showHelp")
+const Queue = require("bee-queue");
+const { chmod } = require('fs');
+const redis = require("redis")
+const { sleep } = require('../gameFunctions/helperFunctions/scriptHelpers')
+
+// Setup bee-queue for commands
+const queueConfig = { redis: "redis://redis:6379" }
+const commandQueue = new Queue('command', queueConfig)
+commandQueue.process(async (job, done) => {
+    console.log("\x1b[36m", job.data.userId + ": " + job.data.command,)
+    let returnMsg = await command(job.data.command, job.data.userId)
+    let returnMsgList = Object.entries(returnMsg)
+    for (let i = 0; i < returnMsgList.length; i++) {
+        for (let j = 0; j < returnMsgList[i][1].length; j++) {
+            await io.to(returnMsgList[i][1][j]).emit('chat message', returnMsgList[i][0])
+        }
+    };
+})
+
+// setup bee-queue for user creation
+const userCreateQueue = new Queue('userCreate', queueConfig)
+userCreateQueue.process(async (job, done) => {
+    await createPerson(job.data)
+})
+
 
 async function setupSocket() {
-
     await io.on('connection', async (socket) => {
         await socket.on('userinfo', async (userInfo) => {
             const userId = userInfo[0]
             const userName = userInfo[1].toLowerCase()
             console.log('User logon and Id created: ' + userId)
             socket.userId = userId
-            
+
             await socket.join(userId)
             socket.room = "lobby"
-            io.to('lobby').emit('chat message', textHelpers.capitalizeFirstLetter(userName) + " has connected." )
+            io.to('lobby').emit('chat message', textHelpers.capitalizeFirstLetter(userName) + " has connected.")
             await socket.join('lobby')
-            await createPerson({
+
+            await userCreateQueue.createJob({
                 roomName: "mud_bedroom",
                 names: [userName, "person"],
                 userName: userName,
                 description: "It's your friend " + textHelpers.capitalizeFirstLetter(userName) + ".",
                 userId: userId
-            })
+            }).save()
+
+            await sleep(500)
+
             const roomObj = await getObjs.getRoomObjByRoomName("mud_bedroom")
             const roomDescribe = await lookFunctions[roomObj.look](userId, roomObj)
             const message = "<br><b>" + textHelpers.capitalizeFirstLetter(roomObj.roomTitle) + "</b><br>" + roomDescribe
@@ -42,30 +70,25 @@ async function setupSocket() {
 
         await socket.on('chat message', async (msg) => {
             // msg is an array of [user submitted message, userid]
-            let returnMsg = await command(msg[0], socket.userId)
-            let returnMsgList = Object.entries(returnMsg)
-            for (let i = 0; i < returnMsgList.length; i++) {
-                for (let j = 0; j < returnMsgList[i][1].length; j++) {
-                    await io.to(returnMsgList[i][1][j]).emit('chat message', returnMsgList[i][0])
-                }
-            };
+            // send command to bee-queue
+            await commandQueue.createJob({ command: msg[0], userId: msg[1] }).save()
         });
 
         await socket.on('disconnect', async () => {
             if (socket.userId) {
                 console.log('user disconnected: ' + socket.userId);
                 const userName = await getUsers.getUserNameByUserId(socket.userId)
-                await io.to('lobby').emit('chat message', textHelpers.capitalizeFirstLetter(userName) + " has disconnected." )
-                
+                await io.to('lobby').emit('chat message', textHelpers.capitalizeFirstLetter(userName) + " has disconnected.")
+
                 // move all items in user inventory to ground
                 const userInv = await getObjs.getAllObjectsInInventory(socket.userId)
                 const roomName = await getUsers.getUserRoomByUserId(socket.userId)
                 const floorObjList = await getObjs.getAllObjsByNameInRoom("floor", roomName)
                 let floorObj = floorObjList[0]
-                for (let i=0;i < userInv.length; i++) {
+                for (let i = 0; i < userInv.length; i++) {
                     // Add items to floor container
                     await floorObj.contains.push(userInv[i])
-                    await setObjs.setObjectPropertyByDbIdAndRoomName(floorObj._id, roomName, 'contains', floorObj.contains)  
+                    await setObjs.setObjectPropertyByDbIdAndRoomName(floorObj._id, roomName, 'contains', floorObj.contains)
                     // set items as visible
                     await setObjs.setObjectPropertyByDbIdAndRoomName(userInv[i]._id, "userInventory_" + socket.userId, 'visible', true)
                     // move items from user inventory to room 
@@ -73,7 +96,7 @@ async function setupSocket() {
                     // add container obj id to floors "inside" property
                     await setObjs.setObjectPropertyByDbIdAndRoomName(userInv[i]._id, roomName, 'inside', String(floorObj._id))
                 }
-                
+
                 // Delete person
                 await deletePerson(socket.userId)
                 await socket.leave(socket.userId)
@@ -87,11 +110,11 @@ async function setupSocket() {
 
         await socket.on('userquery', async (queryInfo) => {
             const userObjList = await getObjs.getAllPeopleInRoom("mud_bedroom")
-            const userNameList = userObjList.map((obj) => {return obj.names[0]})
+            const userNameList = userObjList.map((obj) => { return obj.names[0] })
             if (userNameList.includes(queryInfo[0].toLowerCase())) {
-                await io.emit('query_'+queryInfo[1], 'duplicate')
+                await io.emit('query_' + queryInfo[1], 'duplicate')
             } else {
-                await io.emit('query_'+queryInfo[1], 'no_duplicate')
+                await io.emit('query_' + queryInfo[1], 'no_duplicate')
             }
         });
     });
@@ -103,6 +126,7 @@ async function setupSocket() {
         console.log('-----')
     });
 }
+
 
 module.exports = {
     setupSocket
